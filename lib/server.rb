b7af851d387489359186ce1d0d61a2e669e33d2a
slimecog <wlcjohnson@gmail.com>
2017-12-18 20:07:30 -0700

    iteration one complete

--- lib/server.rb
+++ lib/server.rb
@@ -1,29 +1,39 @@
 require 'socket'
+
 tcp_socket = TCPServer.new(9292)
 counter = 0
+
 loop do
-socket = tcp_socket.accept
+  socket = tcp_socket.accept
+  puts "Ready for a request"
+  request_lines = []
+  while line = socket.gets and !line.chomp.empty?
+    request_lines << line.chomp
+  end
 
-puts "Ready for a request"
-request_lines = []
-while line = socket.gets and !line.chomp.empty?
-  request_lines << line.chomp
-end
+  puts "Got this request:"
+  puts request_lines.inspect
+  counter += 1
+  response = "<pre>" + request_lines.join("\n") + "</pre>"
+  output = "<html><head></head><body><pre>
+  Verb: POST
+  Path: /
+  Protocol: HTTP/1.1
+  Host: 127.0.0.1
+  Port: 9292
+  Origin: 127.0.0.1
+  Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+  </pre>Hello, World! (#{counter})</body></html>"
 
-puts "Got this request:"
+  headers = ["http/1.1 200 ok",
+            "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+            "socket: ruby",
+            "content-type: text/html; charset=iso-8859-1",
+            "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+  socket.puts headers
+  socket.puts output
+  socket.close
 
-puts request_lines.inspect
-counter += 1
-response = "hello world (#{counter})"
-output = "<html><head></head><body>#{response}</body></html>"
-headers = ["http/1.1 200 ok",
-          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-          "socket: ruby",
-          "content-type: text/html; charset=iso-8859-1",
-          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-socket.puts headers
-socket.puts output
-end
 puts ["Wrote this response:", headers, output].join("\n")
-socket.close
 puts "\nResponse complete, exiting."
+end
